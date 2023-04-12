acbffb37e16549a034a26a9fc4f28e801a75b5d0
musawakiliML <musawakiliml@hotmail.com>
2023-04-12 16:38:47 +0100

    Feat: Add Review Model

--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,3 +8,14 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+
+
+class Review(models.Model):
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    watchAgain = models.BooleanField()
+    
+    def __str__(self) -> str:
+        return self.text
